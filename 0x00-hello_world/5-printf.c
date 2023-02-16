32a4f69d5945a5ff4e50dfde16f40c2c26cbd9f4
Rock <todanrockson@gmail.com>
2023-02-16 09:00:34 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints using printf function
+ * Return:0 (Success)
+ */
 
+int main(viod)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
